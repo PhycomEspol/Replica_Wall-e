a6511a577a84da644cac83106e4470711b30d453
Gpozo42 <87042047+Gpozo42@users.noreply.github.com>
2021-08-01 02:29:56 -0500

    Corrección
    Error de switch abierto en evaluarSerial corregido

--- Wall-e/walle-phycom.ino
+++ Wall-e/walle-phycom.ino
@@ -48,43 +48,37 @@ void setup() {
 // Para análisis de señales Bluetooth
 // --------------------------------------------------------------------------
 void evaluarSerial(){
-
-  switch(primerChar) {
-
-    // Movimiento de motores
-    if (primerChar == "w") {
-      motorI.setVelocidad(255);
-      motorD.setVelocidad(255);
-    }
-    else if (primerChar == "a") {
-      motorI.setVelocidad(0);
-      motorD.setVelocidad(255);
-    }
-    else if (primerChar == "s") {
-      motorI.setVelocidad(-255);
-      motorD.setVelocidad(-255);
-    }
-    else if (primerChar == "d") {
-      motorI.setVelocidad(255);
-      motorD.setVelocidad(0);
-    }
-
-    // Movimiento de servos
-    else if (isdigit(primerChar)) {  // Revisamos que los datos sean númericos
-      if (atoi(primerChar) <= 180 && atoi(primerChar) >=0) { // Entre 0 y 180 grados
-        //servoI.write(primerChar); Todavía no establecida la diferencia entre ambos brazos desde la app
-        //servoD.write(primerChar);
-      }
-    }
-
-    // Sin movimiento
-    else {
-      motorI.setVelocidad(0);
-      motorD.setVelocidad(0);
-    }
-    
+  // Movimiento de motores
+  if (primerChar == "w") {
+    motorI.setVelocidad(255);
+    motorD.setVelocidad(255);
   }
-  
+  else if (primerChar == "a") {
+    motorI.setVelocidad(0);
+    motorD.setVelocidad(255);
+  }
+  else if (primerChar == "s") {
+    motorI.setVelocidad(-255);
+    motorD.setVelocidad(-255);
+  }
+  else if (primerChar == "d") {
+    motorI.setVelocidad(255);
+    motorD.setVelocidad(0);
+  }
+
+  // Movimiento de servos
+  else if (isdigit(primerChar)) {  // Revisamos que los datos sean númericos
+    if (atoi(primerChar) <= 180 && atoi(primerChar) >=0) { // Entre 0 y 180 grados
+      //servoI.write(primerChar); Todavía no establecida la diferencia entre ambos brazos desde la app
+      //servoD.write(primerChar);
+    }
+  }
+
+  // Sin movimiento
+  else {
+    motorI.setVelocidad(0);
+    motorD.setVelocidad(0);
+  }
 }
 // --------------------------------------------------------------------------
 //
