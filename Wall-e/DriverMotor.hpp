6b24e16d43a188b983ec5e1a93e0ddebad463a00
Gpozo42 <87042047+Gpozo42@users.noreply.github.com>
2021-08-05 18:02:41 -0500

    Actualización 2
    Se realizaron los siguientes cambios:
    > Clase DriverMotor:
    - Se cambio completamente la función "setVelocidad" por "setDireccion"
    > Main:
    - Se añadió movimientos para servos
    - Se añadió función para separar letras de números
    - Se realizaron cambios de datos en las variables 
    - primerChar pasa a ser primerDato

--- Wall-e/DriverMotor.hpp
+++ Wall-e/DriverMotor.hpp
@@ -1,66 +1,61 @@
 // Para controlar los motores, usando L298N
+
 /*
- * currentPin = Habilita la corriente
- * dirPin = Produce movimiento hacia el frente
- * retroPin = Produce retroceso
+ * currentPin = Habilita la corriente/Enabler
+ * dirPin = Produce movimiento hacia el frente/IN
+ * retroPin = Produce retroceso/IN Inverso
  * 
  */
 
 class DriverMotor{
   public:
     // Constructor
-    DriverMotor(uint8_t currentPin, uint8_t dirPin, uint8_t retroPin);
+    DriverMotor(uint8_t _currentPin, uint8_t _dirPin, uint8_t _retroPin);
     
     // Constructor por defecto
     ~DriverMotor();
 
-    void setVelocidad(int pwmNum);
+    void setDireccion(int direccion);
 
   private:
-    uint8_t this->currentPin, this->dirPin, this->retroPin;
-    bool this->noMove;
+    uint8_t currentPin, dirPin, retroPin;
   
-}
+};
 
 // Definiciones
-DriverMotor::DriverMotor(uint8_t currentPin, uint8_t dirPin, uint8_t retroPin) {
-  this->currentPin = currentPin;
-  this->dirPin = dirPin;
-  this->retroPin = retroPin;
+DriverMotor::DriverMotor(uint8_t _currentPin, uint8_t _dirPin, uint8_t _retroPin) {
+  currentPin = _currentPin;
+  dirPin = _dirPin;
+  retroPin = _retroPin;
 
   pinMode(currentPin, OUTPUT);  // Iniciando el pin de electricidad al motor
   pinMode(dirPin, OUTPUT);
   pinMode(retroPin, OUTPUT);
 
-  this->noMove = false;
 }
 
 DriverMotor::~DriverMotor(){
   // Nada
 }
 
-void DriverMotor::setVelocidad(int pwmValor) { // De 0 a 255
-  // Para regular la velocidad
-  if (pwmValor>255) pwmValor = 255;
-  else if (pwmValor<-255) pwmValor = -225;
+void DriverMotor::setDireccion(int direccion) { // De 0 a 255
+  switch (direccion) {
 
-  // Control de velocidad
-  if (pwmValor > 0) {
-    analogWrite(currentPin, pwmValor);
-    digitalWrite(dirPin, HIGH);
-    digitalWrite(retroPin, LOW;
-    noMove = true;
-  }
-  else if (pwmValor < 0) {
-    analogWrite(currentPin, pwmValor);
-    digitalWrite(dirPin, LOW);
-    digitalWrite(retroPin, HIGH);
-    noMove = true;
-  }
+    case 1: //forward
+      digitalWrite(currentPin, HIGH);
+      digitalWrite(dirPin, HIGH);
+      digitalWrite(retroPin, LOW);
+      break;
+    case 2: //backward
+      digitalWrite(currentPin, HIGH);
+      digitalWrite(dirPin, LOW);
+      digitalWrite(retroPin, HIGH);
+      break;
 
-  //Put something here, if the car is stopped
-  else if (pwmValor == 0) {
-    analogWrite(currentPin, pwmValor);
-    noMove = false;
+    default: //still
+      digitalWrite(currentPin, LOW);
+      break;
+
   }
+ 
 }
--- Wall-e/walle-phycom.ino
+++ Wall-e/walle-phycom.ino
@@ -5,20 +5,20 @@
 // Setting up variables
 
 // Pines para los motores
-#define DIRECCION_D_PIN 5  //IN3
-#define RETRO_D_PIN 4  //IN4
+#define DIRECCION_D_PIN 6  //IN3
+#define RETRO_D_PIN 5  //IN4
 #define PWM_VEL_D_PIN 3  //ENB
-#define DIRECCION_I_PIN 8 //IN1
-#define RETRO_I_PIN 7  //IN2
-#define PWM_VEL_I_PIN 9  //ENA
+#define DIRECCION_I_PIN 10 //IN1
+#define RETRO_I_PIN 9  //IN2
+#define PWM_VEL_I_PIN 11  //ENA
 
 // Pines para módulo HC05
 #define TX_ARDUINO 0
 #define RX_ARDUINO 1
 
 // Pines para servos
-#define SERVO_D_PIN 10
-#define SERVO_I_PIN 11
+#define SERVO_D_PIN 8
+#define SERVO_I_PIN 7
 Servo servoD;
 Servo servoI;
 
@@ -28,7 +28,7 @@ DriverMotor motorD(PWM_VEL_D_PIN, DIRECCION_D_PIN, RETRO_D_PIN);
 
 // Parseo de la señal
 #define MAX_SERIAL_LENGTH 5 // No podemos dejar que se sobrecargue la señal
-char primerChar;
+String primerDato;
 uint8_t serialLength = 0;
 char serialBuffer[MAX_SERIAL_LENGTH];
 
@@ -38,9 +38,9 @@ char serialBuffer[MAX_SERIAL_LENGTH];
 
 void setup() {
   // put your setup code here, to run once:
-  servoI.attach(SERVO_D_PIN);
-  servoD.attach(SERVO_I_PIN);
-  Serial.begin(38400); // Iniciando slave bluetooth
+  servoI.attach(SERVO_I_PIN);
+  servoD.attach(SERVO_D_PIN);
+  Serial.begin(9600); // Iniciando slave bluetooth
 }
 
 
@@ -49,36 +49,55 @@ void setup() {
 // --------------------------------------------------------------------------
 void evaluarSerial(){
   // Movimiento de motores
-  if (primerChar == "w") {
-    motorI.setVelocidad(255);
-    motorD.setVelocidad(255);
+  if (primerDato == "w") {
+    motorI.setDireccion(1);
+    motorD.setDireccion(1);
   }
-  else if (primerChar == "a") {
-    motorI.setVelocidad(0);
-    motorD.setVelocidad(255);
+  else if (primerDato == "a") {
+    motorD.setDireccion(1);
+    motorI.setDireccion(0);
   }
-  else if (primerChar == "s") {
-    motorI.setVelocidad(-255);
-    motorD.setVelocidad(-255);
+  else if (primerDato == "s") {
+    motorI.setDireccion(2);
+    motorD.setDireccion(2);
   }
-  else if (primerChar == "d") {
-    motorI.setVelocidad(255);
-    motorD.setVelocidad(0);
+  else if (primerDato == "s") {
+    motorI.setDireccion(1);
+    motorD.setDireccion(0);
   }
 
   // Movimiento de servos
-  else if (isdigit(primerChar)) {  // Revisamos que los datos sean númericos
-    if (atoi(primerChar) <= 180 && atoi(primerChar) >=0) { // Entre 0 y 180 grados
-      //servoI.write(primerChar); Todavía no establecida la diferencia entre ambos brazos desde la app
-      //servoD.write(primerChar);
+  else if (isdigit(primerDato[0])) {  // Revisamos que los datos sean númericos "ANGULO-BI/BD"
+    int angulo = atoi(getValor(primerDato, '-', 0).c_str());
+    String brazo = getValor(primerDato, '-', 1);
+    if (angulo <= 180 && angulo >=45) { // Entre 0 y 180 grados
+      if (brazo.compareTo("bd") == 0) servoD.write(angulo);
+      if (brazo.compareTo("bi") == 0) servoI.write(angulo);
     }
   }
 
   // Sin movimiento
   else {
-    motorI.setVelocidad(0);
-    motorD.setVelocidad(0);
+    motorI.setDireccion(0);
+    motorD.setDireccion(0);
   }
+}
+
+String getValor(String data, char separador, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separador || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
 // --------------------------------------------------------------------------
 //
@@ -87,11 +106,12 @@ void evaluarSerial(){
 
 void loop() {
   // put your main code here, to run repeatedly:
-  char input;
-
-  while (Serial.available() >= 1) {
-     primerChar = Serial.read();
-  }
-
-  evaluarSerial();  
+  while (Serial.available()) {
+     primerDato = Serial.read();
+     Serial.println(primerDato);
+     evaluarSerial();
+  }  
+  delay(10);
+  
+  
 }
